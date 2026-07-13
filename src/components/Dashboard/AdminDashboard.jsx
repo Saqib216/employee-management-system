133993cebcd1868d0d6e8dbe175d16a9049532e4
Saqib Hussnain <saqibmehar847@gmail.com>
2026-07-13 12:13:34 +0500

    Implemented the logout functionality for AdminDashboard too which was skipped previously

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -3,10 +3,10 @@ import Header from '../other/Header'
 import CreateTask from '../other/CreateTask'
 import AllTasks from '../other/AllTasks'
 
-const AdminDashboard = () => {
+const AdminDashboard = ({handleLogout}) => {
   return (
     <div className='m-5'>
-      <Header />
+      <Header handleLogout={handleLogout}/>
       <CreateTask />
       <AllTasks />
     </div>
