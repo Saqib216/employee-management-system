74f99e67e9c2bf37d3e7bbb391fc5056238c9d81
Saqib Hussnain <saqibmehar847@gmail.com>
2026-07-21 11:50:41 +0500

    In Header.jsx, added checks for userName i.e for employee or admin, only the firstname will be displayed instead of full name

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({ handleLogout, employeeData, adminData }) => {
     return (
         <div>
             <div className='py-8 flex justify-between'>
-                <h2 className='text-2xl'>Hello, <span className='tracking-wide font-semibold'>{userName}👋</span></h2>
+                <h2 className='text-2xl'>Hello, <span className='tracking-wide font-semibold'>{(userName === adminData?.name) ? userName.split(' ').slice(0,2).join(' ') : userName.split(' ')[0]}👋</span></h2>
 
                 <Button variant="secondary" id="logout-btn" onClick={handleLogout}>
                     Log out
